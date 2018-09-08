6e2c365c2e37694bb140f456af5b55ae3e4ff730
limmita <akkermanmen@mail.ru>
2018-09-08 21:47:52 +0300

    first commit

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
+
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -28,7 +30,6 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
--- renderer.js
+++ renderer.js
@@ -1,3 +1,41 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// All of the Node.js APIs are available in this pro
+var DG = require('2gis-maps');
+var Datastore = require('nedb');
+var db = new Datastore({filename : 'users'});
+db.loadDatabase();
+
+db.insert({name : "Koli Sterno", year: 1950, _id : 1}); 
+db.find({}, function (err, docs) {
+	console.log(docs);
+});
+
+
+
+
+
+var map = DG.map('map', {
+    'center': [54.98, 82.89],
+    'zoom': 13,
+    fullscreenControl: false,
+    zoomControl: false
+});
+var marker;
+DG.marker([54.98, 82.89]).addTo(map).bindPopup('Я попап!');
+
+map.on('click', function (event) {
+    
+      marker = DG.marker([event.latlng.lat, event.latlng.lng], {
+                    draggable: true
+                }).addTo(map);
+
+                marker.on('drag', function(e) {
+                    var lat = e.target._latlng.lat.toFixed(3),
+                        lng = e.target._latlng.lng.toFixed(3);
+
+                    console.log('lat', lat)
+                    console.log('lng', lng)
+                });
+    
+  });
