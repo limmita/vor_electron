46d0be995167931c065bfddca776fa22dd757936
limmita <akkermanmen@mail.ru>
2018-12-26 14:02:11 +0200

    full vor

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-var $ = require('jquery')
+var $ = require('jquery') 
 var DG = require('2gis-maps');
 var Datastore = require('nedb');
 const electron = require('electron')
@@ -15,20 +15,141 @@ db.count({}, function (err, doc) {
 
 db.find({}, function (err, docs) {
 });
+var marker, lat, lng, pathFoto, pathFoto_name, pathFile, pathFile_name, status_rang, popur, heder_boss;
+var flag = false;
 
-var map = DG.map('map', {
-    'center': [46.841, 30.800],
-    'zoom': 8,
-    fullscreenControl: false,
-    zoomControl: false,
-    minZoom: 8
-});
+/* добавил для плагина*/
 
+                // загрузка кода модуля
+ // DG.plugin('https://2gis.github.io/mapsapi/vendors/Leaflet.markerCluster/leaflet.markercluster-src.js');
 
 
-var marker, lat, lng, pathFoto, pathFoto_name, pathFile, pathFile_name;
-let flag = false;
+/*-------------------------------------------------------------------*/
 
+
+
+    var map = DG.map('map', {
+        'center': [46.841, 30.800],
+        'zoom': 8,
+        fullscreenControl: false,
+        zoomControl: false,
+        minZoom: 8
+    });
+
+
+    /*------------------------------------------*/
+    // var markers = DG.markerClusterGroup(); /* добавил для плагина*/
+    /*------------------------------------------*/
+
+
+
+
+    db.find({}, function (err, docs) {
+
+            docs.forEach(function(element, index) {
+                /**
+                отображаем на карте иконки
+                */
+                let icSize = [25, 25]
+                switch (element.color) {
+                    case "black":
+                    heder_boss = "Антимос"
+                    break;
+                    case "purple":
+                    heder_boss = "Бєкаєв Омар"
+                    break;
+                    case "red":
+                    heder_boss = "Горадзе Міндія"
+                    break;
+                    case "green":
+                    heder_boss = "Валерий Шеремет"
+                    break;
+                    case "gray":
+                    heder_boss = "Владимир Дрибной"
+                    break;
+                    case "lime":
+                    heder_boss = "Гули"
+                    break;
+                    default:
+                    heder_boss = ""
+                    break;
+                }
+                switch (element.rang) {
+                    case "vor":
+                    iconURL = "assets/icons/"+element.color+"/crown.svg"
+                    status_rang = "Вор в законе"
+                    icSize = [60, 60]
+                    heder_boss =''
+                    break;
+                    case "see":
+                    iconURL = "assets/icons/"+element.color+"/visibility-button.svg"
+                    status_rang = "Смотрящий"
+                    break;
+                    case "posit":
+                    iconURL = "assets/icons/"+element.color+"/hacker.svg"
+                    status_rang = "Положенец"
+                    break;
+                    case "bookk":
+                    iconURL = "assets/icons/"+element.color+"/man-user.svg"
+                    status_rang = "Держит общак"
+                    break;
+                    case "authority":
+                    iconURL = "assets/icons/"+element.color+"/man-user.svg"
+                    status_rang = "Авторитет"
+                    break;
+                    case "bandit":
+                    iconURL = "assets/icons/"+element.color+"/man-user.svg"
+                    status_rang = "Бандит"
+                    break;
+                }
+
+
+                var MyIcon = DG.icon({
+                    iconUrl: iconURL,
+                    iconSize: icSize
+                })
+
+                popur = `<div class="main">
+                <center><h4>${status_rang}</h4>
+                <img src=${element.foto} class="foto">
+                <h4>${element.fio}</h4>
+                <h5>Кличка: ${element.nic}</h5></center>
+                <span class="ref" onclick="
+                document.querySelector('#overlay').style.display = 'block'
+                document.querySelector('#modal_form').style.top = '50%';
+                document.querySelector('#modal_form').style.display = 'block'
+                document.querySelector('#frame').setAttribute('src', '${element.file}')
+                " >справка</span>   #${element._id}
+                </div>`;
+
+                var marker = DG.marker([element.lat, element.lng],{icon: MyIcon})
+                    marker.bindPopup(popur)
+                    marker.bindLabel(`от ${heder_boss}`)
+                    marker.addTo(map);
+                    // markers.addLayer(marker);
+
+                //  if ( element.step != "") {
+
+                //     db.findOne({_id: Number(element.step)}, function (err, geoStep) {
+
+                //         console.log(geoStep);
+                //         poligon = DG.polyline([[element.lat, element.lng], [geoStep.lat, geoStep.lng]],
+                //                             {
+                //                             color: element.color,
+                //                             weight: 3
+                //                                 }).addTo(map);
+                //         });
+                //     //  setTimeout(function() {
+                //     //     poligon.remove(map)
+                //     // }, 5000);
+
+                // }
+            })
+            // map.addLayer(markers);
+
+
+
+    });
 
 
 map.on('click', function (event) {
@@ -46,15 +167,15 @@ map.on('click', function (event) {
                     console.log(lat);
                     console.log(lng);
                 });
-         flag = false;     
-    }  
+         flag = false;
+    }
 
   });
 
 /*
 Открываем справа окно с формой
 */
-$('.icon-arrow-right').on("click",function(){    
+$('.icon-arrow-right').on("click",function(){
     $('#map').addClass("col-8")
     $('form').css("display", "block");
     $('.icon-arrow-right').hide()
@@ -81,8 +202,8 @@ let obj_Man = {
 */
 let sing_flag = true
 $('.sing').on('click', function(event) {
-    
-     
+    // event.preventDefault();
+
     if ($('#fio').val() === "") {
         alert("Заполните поле ФИО")
         sing_flag = false
@@ -101,7 +222,7 @@ $('.sing').on('click', function(event) {
     obj_Man.lng = lng
     obj_Man.step = $('#step').val()
     obj_Man._id = id
-    
+
     try {
         pathFoto = $(".nameFoto")[0].files[0].path;
         pathFoto_name = $(".nameFoto")[0].files[0].name.split('.');
@@ -134,145 +255,11 @@ $('.sing').on('click', function(event) {
     } catch(e) {
         obj_Man.file = '';
     }
-    
-    
 
-    db.insert(obj_Man); 
+
+
+    db.insert(obj_Man);
     // location.reload();
-    
+
 
 })
-
-
-let status_rang, popur, heder_boss;
-db.find({}, function (err, docs) {
-
-    
-    if(!err) {
-        
-        docs.forEach(function(element, index) {
-            
-
-            /**
-            отображаем на карте иконки
-            */
-            let icSize = [25, 25]
-            switch (element.color) {
-                case "black":
-                    heder_boss = "Антимос"
-                    break;
-                case "purple":
-                    heder_boss = "Бєкаєв Омар"
-                    break; 
-                case "red":
-                    heder_boss = "Горадзе Міндія"
-                    break; 
-                case "green":
-                    heder_boss = "Валерий Шеремет"
-                    break; 
-                case "gray":
-                    heder_boss = "Владимир Дрибной"
-                    break;
-                case "lime":
-                    heder_boss = "Гули"
-                    break;                 
-                default:
-                    heder_boss = ""
-                    break;
-            }
-            switch (element.rang) {
-                case "vor":
-                    iconURL = "assets/icons/"+element.color+"/crown.svg"
-                    status_rang = "Вор в законе"
-                    icSize = [60, 60]
-                    heder_boss =''
-                    break;
-                case "see":
-                    iconURL = "assets/icons/"+element.color+"/visibility-button.svg"
-                    status_rang = "Смотрящий"
-                    break;
-                case "posit":
-                    iconURL = "assets/icons/"+element.color+"/hacker.svg"
-                    status_rang = "Положенец"
-                    break; 
-                case "bookk":
-                    iconURL = "assets/icons/"+element.color+"/man-user.svg"
-                    status_rang = "Держит общак"
-                    break;
-                case "authority":
-                    iconURL = "assets/icons/"+element.color+"/man-user.svg"
-                    status_rang = "Авторитет"
-                    break;
-                case "bandit":
-                    iconURL = "assets/icons/"+element.color+"/man-user.svg"
-                    status_rang = "Бандит"
-                    break;                   
-                // default:
-                    
-                //     break;
-            }
-
-
-            var MyIcon = DG.icon({
-                        iconUrl: iconURL,
-                        iconSize: icSize
-                    })
-
-            
-
-            
-           
-
-            popur = `<div class="main">
-                <center><h4>${status_rang}</h4>
-                <img src=${element.foto} class="foto">
-                <h4>${element.fio}</h4>
-                <h5>Кличка: ${element.nic}</h5></center>
-                 <span class="ref" onclick="
-                  document.querySelector('#overlay').style.display = 'block'
-                  document.querySelector('#modal_form').style.top = '50%';
-                  document.querySelector('#modal_form').style.display = 'block'
-                  document.querySelector('#frame').setAttribute('src', '${element.file}')
-
-                            " >справка</span>   #${element._id}
-                </div>`
-
-              
-     
-         
-
-
-
-               
-            DG.marker([element.lat, element.lng],{icon: MyIcon}).addTo(map).bindPopup(popur).bindLabel(`от ${heder_boss}`);
-             
-            
-   
-
-            //  if ( element.step != "") {
-                
-            //     db.findOne({_id: Number(element.step)}, function (err, geoStep) {
-
-            //         console.log(geoStep);
-            //         poligon = DG.polyline([[element.lat, element.lng], [geoStep.lat, geoStep.lng]], 
-            //                             {
-            //                             color: element.color,
-            //                             weight: 3
-            //                                 }).addTo(map);
-            //         });
-            //     //  setTimeout(function() {
-            //     //     poligon.remove(map)
-            //     // }, 5000);
-
-            // }
-        })
-
-
-
-
-
-
-     }
-
-});
- 
